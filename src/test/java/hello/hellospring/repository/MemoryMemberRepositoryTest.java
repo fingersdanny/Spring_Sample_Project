9317d813c090ce42a18c6d36c71d7c05af7a509f
isevou__ <kbry12@naver.com>
2023-03-31 02:08:18 +0900

    Test: MemoryMemberRepository method

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -2,14 +2,27 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class MemoryMemberRepositoryTest {
 
-    MemberRepository repository = new MemoryMemberRepository();
+    MemoryMemberRepository repository = new MemoryMemberRepository();
 
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    // Test를 작성할때는 각각 독립적으로 실행할 수 있도록 해야함
+    // 실행하고 남아있는 데이터를 지우지 않으면 다른 테스트에서 오류가 나올수도
+    // 그래서 각 테스트 코드를 실행하고 데이터를 지워야함
+
+    //TDD: 테스트를 먼저 만들고 그 다음에 테스트를 통해 검증할 대상을 만든다
     @Test
     public void save() {
         Member member = new Member();
@@ -34,6 +47,22 @@ public class MemoryMemberRepositoryTest {
         Member result = repository.findByName("spring1").get();
 
         assertThat(result).isEqualTo(member1);
+
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
 
     }
 }
