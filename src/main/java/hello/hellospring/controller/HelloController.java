65e5695505cd06fe8d40d9574054c082a5963e95
isevou__ <kbry12@naver.com>
2023-03-30 21:59:54 +0900

    Added API, MVC and Template, Domain, Repository, Test

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,13 +4,51 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "spring!");
+        model.addAttribute("data", "hello!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    // html을 변환하지 않고 직접 데이터를 넣는다.
+    @ResponseBody // html을 http에서 head부와 body부가 있는데 body부에 데이터를 직접 넣겠다.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; //"hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+
+    // json으로 반환하게 기본 설정되어있다구.
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+        // xml 방식 <html> ... </html> 되게 무겁고 열고 닫고 두번 써야함;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
