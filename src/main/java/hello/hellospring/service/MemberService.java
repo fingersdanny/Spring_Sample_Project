e6712d74b3ba69f011273f4a2b1e651ca1d39f03
isevou__ <kbry12@naver.com>
2023-03-31 02:48:55 +0900

    Feat: 회원가입 및 전체 회원 조회 추가

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,44 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원X
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+        // get으로 꺼낼 수 있어  값이 없으면 넣고 꺼내  orElseGet
+                    .ifPresent(member1 -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    //롤에 맞도록 네이밍을 한다. 서비스는 비개발자와 함께 볼 수 있도록 이해가는 네이밍
+    // 저장소는 조금 더 개발적인 네이밍
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
