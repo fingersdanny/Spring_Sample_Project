dab25f9b520b440d3792cb8f8ffc0e986bc4832a
isevou__ <kbry12@naver.com>
2023-04-09 00:03:19 +0900

    TimeTraceAop 추가

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -26,17 +26,9 @@ public class MemberService {
     public Long join(Member member) {
 
         long start = System.currentTimeMillis();
-
-        try {
-            // 같은 이름이 있는 중복 회원X
-            validateDuplicateMember(member);
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
-        }
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -54,13 +46,7 @@ public class MemberService {
     // 저장소는 조금 더 개발적인 네이밍
     public List<Member> findMembers() {
         long start = System.currentTimeMillis();
-        try {
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
-        }
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring.service;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,8 @@ public class SpringConfig {
         return new MemberService(memberRepository);
 
     }
+
+    // 이렇게 해주면 Aop가 쓰이는줄 다른 사람들이 알 수 있음
 
     // Spring Bean에 등록된 memberRepository를 넣어서 기존의 Component Scan일때 처럼 연결 해줌
 //    @Bean
